0921cdac4a69273336214796a8bd65c5c852e7ce
HJZR2004 <A01027743@tec.mx>
2025-05-24 21:37:25 -0600

    feat: add changes on the style

--- 501/vite-project/src/class4/A01027743/render.tsx
+++ 501/vite-project/src/class4/A01027743/render.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import TravelRequestForm from './pages/TravelRequestForm'
-import ExpenseForm from './pages/ExpenseForm'
+import './render.css'; // Import the CSS file
+import TravelRequestForm from './pages/TravelRequestForm';
+import ExpenseForm from './pages/ExpenseForm';
 
 createRoot(document.getElementById('class4')!).render(
   <StrictMode>
-    <TravelRequestForm/>
-    <ExpenseForm/>
+    <button className='back-to' onClick={() => window.location.href = '/A01027743/menu.html'}>menu</button>
+    <div className="container">
+      <TravelRequestForm />
+      <ExpenseForm />
+    </div>
   </StrictMode>,
-)
+);
